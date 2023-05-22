03a9813ef856002f512213274ff4c306cc81214e
MAC <hrfunsojoba@gmail.com>
2023-05-22 18:26:26 +0100

    Dockerization

--- src/page_tracker/app.py
+++ src/page_tracker/app.py
@@ -1,8 +1,12 @@
-import os
-from flask import Flask
-from functools import cache
-from redis import Redis, RedisError
+"""
+    This is the home function for the app
+"""
 
+import os
+from functools import cache
+
+from flask import Flask
+from redis import Redis, RedisError
 
 app = Flask(__name__)
 # redis = Redis()
@@ -10,19 +14,23 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
+    """
+        The home function, returns the view count of the page
+    """
     try:
         page_count = redis().incr("page_count")
     except RedisError:
         app.logger.exception("Redis error")
         return "Sorry, something went wrong \N{pensive face}", 500
-    else:
-        times = "times" if page_count > 1 else "time"
-        return f"This page has been viewed {page_count} {times}"
-
+    times = "times" if page_count > 1 else "time"
+    return f"This page has been viewed {page_count} {times}"
 
 
 @cache
 def redis():
+    """
+        Abstracts the redis class
+    """
     return Redis.from_url(os.getenv("REDIS_URL", "redis://localhost:6379"))
 
 
